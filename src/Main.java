bd9bf997eedd390affc513e8bafc35bd396377be
bezhan5lion <bezhanomari.bo@gmail.com>
2024-11-19 00:18:19 -0500

    3rd Commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 7; i++) {
             System.out.println("Hello and welcome!");
             Thread.sleep(2000);
         }
